7a6e5198112f080cd4f564439eb300fdc795317c
Wassim Akkari <was.akkari@gmail.com>
2024-10-12 20:11:11 +0100

    add zig implementation of 3D matrix

--- hello.zig
+++ hello.zig
@@ -21,6 +21,7 @@ pub fn main() void {
         .{ .{ 3, 4 }, .{ 4, 5 }, .{ 5, 6 } },
         .{ .{ 6, 7 }, .{ 7, 8 }, .{ 8, 9 } },
     };
+
     // Print the matrix
     for (matrix) |row| {
         for (row) |pair| {
